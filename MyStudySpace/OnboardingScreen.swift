e629ceaee089a4e50838c4b4d5f89892f3d2779e
XIaohu He <hxh3568579@gmail.com>
2020-04-10 14:57:52 -0400

    Finished OnboardingScreen second page and third page
    And changed second page and third page's second from 24 to 20.

--- MyStudySpace/OnboardingScreen.swift
+++ MyStudySpace/OnboardingScreen.swift
@@ -69,24 +69,24 @@ extension OnboardingScreen: PaperOnboardingDataSource {
                              descriptionFont: UIFont.systemFont(ofSize: 20)),
 
          OnboardingItemInfo(informationImage: #imageLiteral(resourceName: "technical-support"),
-                                        title: "Page 2",
-                                  description: "description",
+                                        title: "Help us improve",
+                                  description: "We are always looking for ways to improve. Share your unique app experience with our team and help us stay in touch. If you encounter any errors or bugs, you can find us through the “contact us” in the settings, and then send us an error report. These reports can help us improve our application. Thank you very much for your help!",
                                      pageIcon: #imageLiteral(resourceName: "technical-support"),
                                         color: #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1),
                                    titleColor: UIColor.black,
                              descriptionColor: UIColor.black,
                                    titleFont: UIFont.systemFont(ofSize: 30),
-                             descriptionFont: UIFont.systemFont(ofSize: 24)),
+                             descriptionFont: UIFont.systemFont(ofSize: 20)),
 
         OnboardingItemInfo(informationImage: UIImage(named:"search")!,
-                                     title: "Page 3",
-                               description: "description",
+                                     title: "More features",
+                               description: "MyStudySpace has some handy links for Laurier Students, such as the commonly used Visual Schedule Builder, Loris, and MylearningSpace. Students could be accessed these links easily through this app...",
                                   pageIcon: UIImage(named:"search")!,
                                      color: #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1),
                                 titleColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0),
                           descriptionColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0),
                                 titleFont: UIFont.systemFont(ofSize: 30),
-                          descriptionFont: UIFont.systemFont(ofSize: 24))
+                          descriptionFont: UIFont.systemFont(ofSize: 20))
             ][index]
     }
     
